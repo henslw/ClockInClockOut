1178bfd6deeace2e0c4d92185460b44105fa1936
hgchoi1116 <100386876+hgchoi1116@users.noreply.github.com>
2022-05-21 00:20:38 -0700

    Export function working. Just need design cleanup

--- ClockInClockOut/wwwroot/Script.js
+++ ClockInClockOut/wwwroot/Script.js
@@ -1,3 +1,21 @@
 ﻿window.Alert = function (message) {
     alert(message);
 }
+
+async function downloadFileFromStream(fileName, contentStreamReference) {
+    const arrayBuffer = await contentStreamReference.arrayBuffer();
+    const blob = new Blob([arrayBuffer]);
+    const url = URL.createObjectURL(blob);
+
+    triggerFileDownload(fileName, url);
+
+    URL.revokeObjectURL(url);
+}
+
+function triggerFileDownload(fileName, url) {
+    const anchorElement = document.createElement('a');
+    anchorElement.href = url;
+    anchorElement.download = fileName ?? '';
+    anchorElement.click();
+    anchorElement.remove();
+}
